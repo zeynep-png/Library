179b7a18f0e798a9af6ebee84230ec09c7f195d0
Zeynep Çakır <59227119+zeynep-png@users.noreply.github.com>
2024-08-05 10:09:20 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Book
+    {
+        //Proporties for the Book class
+        public string BookName { get; set; }
+        public string WriterFirstName { get; set; }
+        public string WriterLastName { get; set; }
+        public int PaperCount { get; set; }
+        public string Publisher {  get; set; }
+        public DateTime DateTime { get; set; }
+
+        //Default Constructor
+        public Book() { 
+            DateTime = DateTime.Now;
+        }
+
+        //Constructor with parameters
+        public Book(string bookName, string writerFirstName, string writerLastName, int paperCount, string publisher)
+        {
+            
+            BookName = bookName;
+            WriterFirstName = writerFirstName;
+            WriterLastName = writerLastName;
+            PaperCount = paperCount;
+            Publisher = publisher;
+            
+            DateTime = DateTime.Now;
+
+        }
+        //Method to print the book details
+       public void Show()
+        {
+            Console.WriteLine(WriterFirstName + " " + WriterLastName +" adlı yazarın " + PaperCount + " sayfalık " + Publisher + " tarafından çıkarılan " + BookName +" adlı kitabını oluşturdunuz.");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Library;
+
+//Create a book ogject using the default constructor
+Book book1 = new Book();
+book1.BookName = "Bir Kitap";
+book1.WriterFirstName = "Ahmet";
+book1.WriterLastName = "Yılmaz";
+book1.PaperCount = 320;
+book1.Publisher = "Alfa Yayınları";
+//Print the details of the first book using the Show method
+book1.Show();
+
+//Create more book objects using the parameterized constructor
+Book book2 = new Book("Başka Bir Kitap","Ayşe","Karan",215,"Beta Yayıncılık");
+book2.Show();
+Book book3 = new Book("Daha Başka Kitap", "Mehmet", "Demir", 450, "Gamma Yayınları");
+book3.Show();
+Book book4 = new Book("Son Kitap", "Elif", "Şahin", 275, "Delta Yayıncılık");
+book4.Show();
+Book book5 = new Book("Örnek Kitap", "Selin", "Yurt", 198, "Epsilon Yayınları");
+book5.Show();
+
+
+//Class belirli bir nesnenin yapısını ve davranışlarını tanımlayan bir şablondur. Fields, properties ve methods içerir.
+
+//Property bir class ın özelliklerini(fields) dış dünyaya sunmak için kullanılan yapıdır. Get set metodları ile atama ve değer döndürme yapılır.
+
+//New anahtar kelimesi bir sınıfın yeni bir örneğini (instance) oluşturmak için kullanılır.
+
+//Constructor bir sınıfın yeni bir örneği oluştuğunda çağrılan özel bir metottur. Sınıfın başlangıç değerini ayarlar. Parametre alabilir veya almayabilir.
+
